8f472608c47559efd47184bac4e4e05420922678
neilbutler <neil.butler@digital.landregistry.gov.uk>
2015-09-01 16:13:54 +0100

    process and display links to datasets

--- service/server.py
+++ service/server.py
@@ -3,9 +3,7 @@ from flask import abort, render_template, url_for, send_file, request, redirect
 from service import app
 from random import randint
 
-open_datasets = []
-commercial_datasets = ['NSD']
-
+months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 @app.route('/')
 @app.route('/index.htm')
@@ -16,66 +14,34 @@ def index():
 @app.route('/data')
 @app.route('/data.html')
 def data():
-    return render_template('data.html')
+    # Get the link
+    files=[]
+    dic1 = {"filename":"ov_full_2015_08.zip","url":"ov_full_2015_08"}
+    dic2 = {"filename":"ov_updates_2014_07.zip","url":"ov_updates_2014_07"}
+    dic3 = {"filename":"ov_updates_2015_09.zip","url":"ov_updates_2015_09"}
 
-@app.route('/<data_type>/<dataset>')
-def view_commercial_dataset_page(data_type, dataset):
+    files.append(dic1)
+    files.append(dic2)
+    files.append(dic3)
 
-    # Check they are trying to access the datasets via the correct URL e.g. NSD via 'commercial' not 'open'
-    check_valid_dataset_type(data_type, dataset)
+    fullDatasets  = []
+    updatedDatasets = []
 
-    # Retrieve the eSecurity values from the request header
-    esecurity = get_esecurity_header_values()
+    for link in files:
+        # Split into a list of words and reorder the month and year
+        words = link["filename"].split("_")
+        # Display the month in name format
+        words[3] = months[int(words[3][:2])-1]
 
-    # TEMPORARY
-    # If user is trying to access a commercial dataset send them to the login page if no user_id in request header
-    if not esecurity["user_id"] and data_type.lower() == 'commercial':
-        breadcrumbs = [
-            {"text": "Home", "url": url_for('index', _external=True)},
-            {"text": "Login", "url": ""}
-        ]
-        return render_template('login.html', breadcrumbs=breadcrumbs)
+        print (words)
+        if words[1] == "full":
+            newLink = "Overseas Dataset (" + words[2] + " " + words[3] + ")"
+            fullDatasets.append({"filename":newLink, "url":link["url"]})
+        else:
+            updateLink = "Overseas Dataset (" + words[2] + " " + words[3] + " update)"
+            updatedDatasets.append({"filename":updateLink, "url":link["url"]})
 
-    breadcrumbs = [
-        {"text": "Home", "url": url_for('index', _external=True)},
-        {"text": "Dataset download", "url": ""}
-    ]
-
-    x = randint(1, 5)
-    roles = esecurity["roles"] if esecurity["roles"] else ''
-    return render_template(dataset + ".html", breadcrumbs=breadcrumbs, files=x, roles=roles)
-
-
-@app.route('/<data_type>/<dataset>/<filename>')
-def download_dataset(data_type, dataset, filename):
-
-    # Check they are trying to access the datasets via the correct URL e.g. NSD via 'commercial' not 'open'
-    check_valid_dataset_type(data_type, dataset)
-
-    return send_file("static/Datasets/NSD-Dataset-1.zip",
-                     mimetype="application/zip",
-                     as_attachment=True,
-                     attachment_filename=filename)
-
-
-def check_valid_dataset_type(data_type, dataset):
-    if data_type.lower() == 'commercial':
-        if dataset.upper() not in commercial_datasets:
-            abort(404)
-    elif data_type.lower() == 'open':
-        if dataset.upper() not in open_datasets:
-            abort(404)
-    else:
-        abort(404)
-
-
-def get_esecurity_header_values():
-    header_info = {
-        "user_id": request.headers.get('iv-user'),
-        "roles": request.headers.get('iv-groups')
-    }
-    return header_info
-
+    return render_template('data.html', fullDatasets=fullDatasets, updatedDatasets=updatedDatasets)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
