14abb255f017d46aa7ee51197b6fb634a1af0f55
neilbutler <neil.butler@digital.landregistry.gov.uk>
2015-09-04 09:57:19 +0100

    finish call to backend

--- service/server.py
+++ service/server.py
@@ -2,6 +2,7 @@
 from flask import abort, render_template, url_for, send_file, request, redirect
 from service import app
 from random import randint
+import requests
 
 months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
@@ -14,22 +15,13 @@ def index():
 @app.route('/data')
 @app.route('/data.html')
 def data():
-    response = requests.get['http://localhost:5001/list-files/overseas-ownership']
+    response = requests.get('http://localhost:5001/list-files/overseas-ownership')
 
     # Get the link
-    files = response.json['File list']
-
-#    dic1 = {"filename":"ov_full_2015_08.zip","url":"ov_full_2015_08"}
-#    dic2 = {"filename":"ov_updates_2014_07.zip","url":"ov_updates_2014_07"}
-#    dic3 = {"filename":"ov_updates_2015_09.zip","url":"ov_updates_2015_09"}
-
-#    files.append(dic1)
-#    files.append(dic2)
-#    files.append(dic3)
+    files = response.json()['File_List']
 
     fullDatasets  = []
     updatedDatasets = []
-
 
     for link in files:
         # Split into a list of words and reorder the month and year
@@ -37,13 +29,12 @@ def data():
         # Display the month in name format
         words[3] = months[int(words[3][:2])-1]
 
-        print (words)
         if words[1] == "FULL":
             newLink = "Overseas Dataset (" + words[3] + " " + words[2] + ")"
-            fullDatasets.append({"filename":newLink, "url":link["url"]})
+            fullDatasets.append({"filename":newLink, "url":link["URL"]})
         else:
             updateLink = "Overseas Dataset (" + words[3] + " " + words[2] + " update)"
-            updatedDatasets.append({"filename":updateLink, "url":link["url"]})
+            updatedDatasets.append({"filename":updateLink, "url":link["URL"]})
 
     return render_template('data.html', fullDatasets=fullDatasets, updatedDatasets=updatedDatasets)
 
