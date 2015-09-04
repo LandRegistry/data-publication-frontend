c48971526ecf0c8a286c37319d5754d8a0a6001b
Aron Jasper <aron.jasper@digital.landregistry.gov.uk>
2015-09-04 10:37:14 +0100

    recaptcha test

--- service/server.py
+++ service/server.py
@@ -2,6 +2,7 @@
 from flask import abort, render_template, url_for, send_file, request, redirect
 from service import app
 from random import randint
+import requests
 
 months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
@@ -11,37 +12,41 @@ months = ["January","February","March","April","May","June","July","August","Sep
 def index():
     return render_template('ood.html')
 
-@app.route('/data')
-@app.route('/data.html')
+@app.route('/recaptcha')
+@app.route('/recaptcha.htm')
+@app.route('/recaptcha.html')
+def recaptcha():
+    return render_template('recaptcha.html')
+
+@app.route('/data', methods=['POST'])
+@app.route('/data.html', methods=['POST'])
 def data():
-    # Get the link
-    files=[]
-    dic1 = {"filename":"ov_full_2015_08.zip","url":"ov_full_2015_08"}
-    dic2 = {"filename":"ov_updates_2014_07.zip","url":"ov_updates_2014_07"}
-    dic3 = {"filename":"ov_updates_2015_09.zip","url":"ov_updates_2015_09"}
+    payload = {'secret': '6LegMQwTAAAAAEhlqMetreZndcUni7Sferq-4q1u', 'response': request.form['g-recaptcha-response']}
+    captcha = requests.post('https://www.google.com/recaptcha/api/siteverify', data=payload)
+    print(captcha.json())
+    if captcha.json()['success']:
+        response = requests.get('http://localhost:5001/list-files/overseas-ownership')
+        files = response.json()['File_List']
+        fullDatasets  = []
+        updatedDatasets = []
 
-    files.append(dic1)
-    files.append(dic2)
-    files.append(dic3)
+        for link in files:
+            # Split into a list of words and reorder the month and year
+            words = link["Name"].split("_")
+            # Display the month in name format
+            words[3] = months[int(words[3][:2])-1]
 
-    fullDatasets  = []
-    updatedDatasets = []
+            if words[1] == "FULL":
+                newLink = "Overseas Dataset (" + words[3] + " " + words[2] + ")"
+                fullDatasets.append({"filename":newLink, "url":link["URL"]})
+            else:
+                updateLink = "Overseas Dataset (" + words[3] + " " + words[2] + " update)"
+                updatedDatasets.append({"filename":updateLink, "url":link["URL"]})
 
-    for link in files:
-        # Split into a list of words and reorder the month and year
-        words = link["filename"].split("_")
-        # Display the month in name format
-        words[3] = months[int(words[3][:2])-1]
-
-        print (words)
-        if words[1] == "full":
-            newLink = "Overseas Dataset (" + words[2] + " " + words[3] + ")"
-            fullDatasets.append({"filename":newLink, "url":link["url"]})
-        else:
-            updateLink = "Overseas Dataset (" + words[2] + " " + words[3] + " update)"
-            updatedDatasets.append({"filename":updateLink, "url":link["url"]})
-
-    return render_template('data.html', fullDatasets=fullDatasets, updatedDatasets=updatedDatasets)
+        return render_template('data.html', fullDatasets=fullDatasets, updatedDatasets=updatedDatasets, duration=response.json()['Link_Duration'])
+    else:
+        error = "Failed reCAPTCHA challenge"
+        return render_template('recaptcha.html', error=error)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
