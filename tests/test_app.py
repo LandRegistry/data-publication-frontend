e67de92ca9367d5787bb6499d635405df2b466c1
Aron Jasper <aron.jasper@digital.landregistry.gov.uk>
2015-09-07 14:55:53 +0100

    Add Unit Tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,96 +1,80 @@
 import pytest
 
 from service.server import app
+from tests.fake_response import FakeResponse
 from unittest import mock
+import json
 
-no_role = {"user_id": "sg1dp3000", "roles": ""}
-correct_role = {"user_id": "sg1dp3000", "roles": "NSD"}
-incorrect_role = {"user_id": "sg1dp3000", "roles": "ABC"}
+import requests
+
+recaptcha_pass = {"success": True}
+recaptcha_fail = {"success": False}
+
+multiple_files = {
+    "File_List": [
+        {
+            "Name": "OV_FULL_2015_08.zip",
+            "Size": 45000000,
+            "Checksum": "\"64c7d38f9bb980717c86fa66cd888117\"",
+            "URL": "https://s3.eu-central-1.amazonaws.com/..."
+        },
+        {
+            "Name": "OV_UPDATE_2015_08.zip",
+            "Size": 2500000,
+            "Checksum": "\"7e461444e9d2d959eb035c2528d9848a\"",
+            "URL": "https://s3.eu-central-1.amazonaws.com/..."
+        }
+    ],
+    "Link_Duration": 60
+}
+no_files = {
+    "File_List": [],
+    "Link_Duration": 60
+}
 
 class TestNavigation:
 
     def setup_method(self, method):
         self.app = app.test_client()
 
-    def test_get_index_page(self):
+    def test_get_index_page_success(self):
         response = self.app.get('/')
         content = response.data.decode()
         assert response.status_code == 200
-        assert 'Land Registry data' in content
-        assert 'National Spatial Dataset' in content
+        assert 'Land Registry Data' in content
+        assert 'Overseas Ownership Dataset' in content
 
-    def test_view_ppd_page_with_incorrect_param_combo_without_role_code(self):
-        response = self.app.get('/commercial/ppd', follow_redirects=True)
-        content = response.data.decode()
-        assert response.status_code == 404
-        assert '404: Not Found' in content
-
-    def test_view_nsd_page_with_valid_param_combo_without_login(self):
-        response = self.app.get('/commercial/nsd', follow_redirects=True)
+    def test_get_recaptcha_page_success(self):
+        response = self.app.get('/recaptcha')
         content = response.data.decode()
         assert response.status_code == 200
-        assert 'Login' in content
-        assert 'Username' in content
-        assert 'Password' in content
+        assert 'Land Registry Data' in content
+        assert 'reCAPTCHA Check' in content
 
-    @mock.patch('service.server.get_esecurity_header_values', return_value=no_role)
-    def test_view_nsd_page_with_valid_param_combo_without_role_code(self, mock_header):
-        response = self.app.get('/commercial/nsd', follow_redirects=True)
+    @mock.patch('requests.post', return_value=FakeResponse(str.encode(json.dumps(recaptcha_pass))))
+    @mock.patch('requests.get', return_value=FakeResponse(str.encode(json.dumps(multiple_files))))
+    def test_get_datasets_success_recaptcha_multiple_files(self, mock_backend_reponse, mock_recaptcha_response):
+        response = self.app.get('/data')
         content = response.data.decode()
         assert response.status_code == 200
-        assert 'National Spatial Dataset' in content
-        assert 'Set-up process' in content
+        assert "Overseas Dataset (August 2015)" in content
+        assert "Overseas Dataset (August 2015 update)" in content
 
-    @mock.patch('service.server.get_esecurity_header_values', return_value=correct_role)
-    def test_view_nsd_page_with_valid_param_combo_with_role_code(self, mock_header):
-        response = self.app.get('/commercial/nsd', follow_redirects=True)
+    @mock.patch('requests.post', return_value=FakeResponse(str.encode(json.dumps(recaptcha_pass))))
+    @mock.patch('requests.get', return_value=FakeResponse(str.encode(json.dumps(no_files))))
+    def test_get_datasets_success_recaptcha_no_files(self, mock_backend_reponse, mock_recaptcha_response):
+        response = self.app.get('/data')
         content = response.data.decode()
         assert response.status_code == 200
-        assert 'National Spatial Dataset' in content
-        assert 'Download Dataset' in content
+        assert "Full Datasets" in content
+        assert "Change-Only Updates" in content
 
-    @mock.patch('service.server.get_esecurity_header_values', return_value=incorrect_role)
-    def test_view_nsd_page_with_valid_param_combo_with_incorrect_role_code(self, mock_header):
-        response = self.app.get('/commercial/nsd', follow_redirects=True)
+    @mock.patch('requests.post', return_value=FakeResponse(str.encode(json.dumps(recaptcha_fail))))
+    def test_get_datasets_fail_recaptcha(self, mock_recaptcha_response):
+        response = self.app.get('/data')
         content = response.data.decode()
         assert response.status_code == 200
-        assert 'National Spatial Dataset' in content
-        assert 'Set-up process' in content
-
-    def test_view_nsd_page_with_incorrect_param_combo_without_role_code(self):
-        response = self.app.get('/open/nsd', follow_redirects=True)
-        content = response.data.decode()
-        assert response.status_code == 404
-        assert '404: Not Found' in content
-
-    def test_view_nsd_page_with_invalid_param_combo_without_role_code(self):
-        response = self.app.get('/made-up-data-type/nsd', follow_redirects=True)
-        content = response.data.decode()
-        assert response.status_code == 404
-        assert '404: Not Found' in content
-
-    @mock.patch('service.server.check_valid_dataset_type')
-    def test_view_nsd_page_when_unknown_error_occurs(self, mock_check):
-        mock_check.side_effect = Exception('test exception')
-        response = self.app.get('/commercial/nsd', follow_redirects=True)
-        assert response.status_code == 500
-
-    def test_download_nsd_dataset_with_valid_param_combo_without_role_code(self):
-        response = self.app.get('/commercial/nsd/NSD-Dataset-1.zip')
-        assert response.status_code == 200
-        assert open('service/static/Datasets/NSD-Dataset-1.zip', 'rb').read() == response.data
-
-    def test_download_nsd_dataset_with_incorrect_param_combo_without_role_code(self):
-        response = self.app.get('/open/nsd/NSD-Dataset-1.zip', follow_redirects=True)
-        content = response.data.decode()
-        assert response.status_code == 404
-        assert '404: Not Found' in content
-
-    def test_download_nsd_dataset_with_invalid_param_combo_without_role_code(self):
-        response = self.app.get('/made-up-data-type/nsd/NSD-Dataset-1.zip', follow_redirects=True)
-        content = response.data.decode()
-        assert response.status_code == 404
-        assert '404: Not Found' in content
+        assert "Failed reCAPTCHA challenge" in content
 
 if __name__ == '__main__':
     pytest.main()
