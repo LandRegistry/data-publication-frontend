89dac562b360fdefbabe836631b9c02c9ca3b86c
Warwick Luscombe <warwick.luscombe@landregistry.gsi.gov.uk>
2016-03-04 10:49:48 +0000

    change xml output

--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -3,4 +3,4 @@
 source ./environment.sh
 source ./environment_test.sh
 
-py.test --cov-report html --junit-xml=reports/coverage.xml --cov-report term --cov service tests/
+py.test --cov-report html --cov-report xml --cov-report term-missing --cov service tests/
